08c0121f0506eaf7c3c6791444dd357aa1e3f3c2
Cesar Napoleon Mejia <cmejia@gmail.com>
2022-12-28 10:52:40 +0700

    remove music player

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import 'reset-css'
+import PlayerLayout from '../components/PlayerLayout'
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: '#f5f5f5',
+      200: '#eeeeee',
+      300: '#EOEOEO',
+      400: '#bdbdbd',
+      500: '#9e9e9e',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121',
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ':focus': {
+            outline: 'none',
+            boxShadow: 'none',
+          },
+        },
+      },
+    },
+  },
+})
+const App = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider theme={theme}>
+    <PlayerLayout>
+      <Component {...pageProps} />
+    </PlayerLayout>
+  </ChakraProvider>
+)
 
 export default App
