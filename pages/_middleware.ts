dc051f111e76f3dfea749098ee6486f7f12b3b19
Cesar Mejia <cmejia@gmail.com>
2023-01-01 21:36:06 +0700

    remove logs

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,15 +3,13 @@ import { NextRequest, NextResponse } from 'next/server'
 const signedInPages = ['/', '/playlists', '/library']
 
 const middleware = (req: NextRequest) => {
-  console.log('middleware: launched')
   if (signedInPages.find((page) => page === req.url)) {
     const token = req.cookies.TRAX_ACCESS_TOKEN
-    console.log('token:', token)
 
     if (!token) {
-      console.log('no token:', token)
       return NextResponse.redirect('/auth/signin', 302)
     }
+    return NextResponse.next()
   }
 }
 
