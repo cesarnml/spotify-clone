aac8fe3c15b59ff61fdfd654de7afde402aa12e6
Cesar Mejia <cmejia@gmail.com>
2023-01-04 05:04:35 +0700

    complete lesson 36 (artist cards)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { Box, Flex, Text } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/react'
 import GradientLayout from '@components/GradientLayout'
 import prisma from '@lib/prisma'
 import { Artist } from '@prisma/client'
@@ -17,17 +19,31 @@ const Home = ({ artists }: Props) => {
       </Head>
       <GradientLayout
         isRoundedImage
-        color="green"
+        color="purple"
         subtitle="profile"
         title="Cesar Mejia"
         description="15 public playlist"
         image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
       >
-        <div>
-          {artists.map((artist) => (
-            <div key={artist.id}>{artist.name}</div>
-          ))}
-        </div>
+        <Box color="white" paddingX="40px">
+          <Box marginBottom="40px">
+            <Text fontSize="2xl" fontWeight="bold">
+              Top artist this month
+            </Text>
+            <Text fontSize="md">only visible to you</Text>
+          </Box>
+          <Flex gap="20px">
+            {artists.map((artist) => (
+              <Box key={artist.id} bg="gray.800" borderRadius="4px" padding="15px" width="20%">
+                <Image src="https://placekitten.com/300/300" borderRadius="100%" alt="artist" />
+                <Box marginTop="20px">
+                  <Text fontSize="xl">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            ))}
+          </Flex>
+        </Box>
       </GradientLayout>
     </>
   )
@@ -35,7 +51,6 @@ const Home = ({ artists }: Props) => {
 
 export const getServerSideProps = async () => {
   const artists = await prisma.artist.findMany({ select: { id: true, name: true } })
-  console.log('artists:', artists)
 
   return {
     props: {
