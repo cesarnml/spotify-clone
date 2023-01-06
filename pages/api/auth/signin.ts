0e5ef7a3beb2d59dc687f4c25a64937d6b897446
Cesar Mejia <cmejia@gmail.com>
2023-01-06 08:48:40 +0700

    fix ts error that is preventing deploy

--- pages/api/auth/signin.ts
+++ pages/api/auth/signin.ts
@@ -53,7 +53,8 @@ handler.post(async (req, res) => {
     const payload = {
       id: user.id,
       email: user.email,
-      name: user.name,
+      firstName: user.firstName,
+      lastName: user.lastName,
       createdAt: user.createdAt,
       updatedAt: user.updatedAt,
       iat: Date.now(),
